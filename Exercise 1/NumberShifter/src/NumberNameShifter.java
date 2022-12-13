dd3f7b406f9721ef1400d3238e54af4d455461cf
baraa <baraashaiban@gmail.com>
2022-12-13 18:13:53 +0100

    first commit

---
+++ Exercise 1/NumberShifter/src/NumberNameShifter.java
@@ -0,0 +1,20 @@
+public class NumberNameShifter {
+    static final String[] numbersInWords = {
+
+            " one",
+            " two",
+            " three",
+            " four",
+            " five",
+            " six",
+            " seven",
+            " eight",
+            " nine"
+    };
+    public void numsToWords(){
+        for (int i = 1; i < 10; i++) {
+
+        }
+    }
+
+}
